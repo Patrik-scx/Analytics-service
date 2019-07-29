0947fb13d6d67fa1107b52d8dae587f29c92e043
ivan-kuzma <email>
2019-07-29 10:38:33 +0300

    fix typo

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 1) set PG_CONNECTION_STRING in .env
 2) npm run db:migrate
 3) npm run start:dev
-4) navigato to localhost:3000/api/docs
+4) navigate to localhost:3000/api/docs
--- app/middlewares/errorHandler.js
+++ app/middlewares/errorHandler.js
@@ -1,7 +1,6 @@
 const env = process.env.NODE_ENV;
 
-// eslint-disable-next-line no-unused-vars
-module.exports = (err, req, res, next) => {
+module.exports = (err, req, res) => {
   const error = env === 'production' ? 'Internal server error' : err.message || err;
   return res.status(500).json({ error });
 };
